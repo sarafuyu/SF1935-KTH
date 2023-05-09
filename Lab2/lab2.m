3ffcb4959b882ccdbc7dfcef383abd4b9245b2ee
Sara Rydell <sryde@kth.se>
2023-05-09 16:14:47 +0200

    Add answer to problem 5

--- Lab2/lab2.m
+++ Lab2/lab2.m
@@ -126,30 +126,59 @@
 
 %% Problem 5 - Test av normalitet
 
-
-load birth.dat
-x = birth(birth(:, 20) < 3, 3);
-y = birth(birth(:, 20) == 3, 3);
-
-% Använd length för att se storleken på vektorerna x och y
-x_length = length(x) % det här ger längden på vektorn x
-y_length = length(y) % det här ger längden på vektorn y
-whos x y; %% för att se detaljer om vektorerna x och y
-
-% Problem 4: Fordelningar av givna data (forts.)
-subplot(2,2,1), boxplot(x),
-axis([0 2 500 5000])
-subplot(2,2,2), boxplot(y),
-axis([0 2 500 5000])
-
-% Problem 4: Fordelningar av givna data (forts.)
-subplot(2,2,3:4), ksdensity(x),
-hold on
-[fy, ty] = ksdensity(y);
-plot(ty, fy, 'r')
-hold off
-
-
+% Ladda upp datan till MATLAB från 'birth.dat'
+filename = 'birth.dat';
+fileID = fopen(filename, 'rt');
+rawData = fread(fileID, '*char')';
+fclose(fileID);
+% Replace consecutive delimiters with a single space
+cleanData = regexprep(rawData, ' +', ' ');
+% Read specific columns from the cleaned data
+formatSpec = '%*f %*f %*f %f %f %*f %*f %*f %*f %*f %*f %*f %*f %*f %*f %f %f %*[^\n]';
+dataArray = textscan(cleanData, formatSpec, ...
+'Delimiter', ' ', ...
+'CollectOutput', true);
+data = cell2mat(dataArray);
+% Extrahera de relevanta kolumnerna för de fyra variablerna
+fodelsevikt = data(:,1);
+mors_alder = data(:,2);
+mors_vikt = data(:,3);
+mors_langd = data(:,4);
+% Använder `jbtest` för att utföra Jarque-Bera-testet på varje kolumn och lagrar resultaten.
+alpha = 0.05;
+[h1, p1] = jbtest(fodelsevikt);
+[h2, p2] = jbtest(mors_alder);
+[h3, p3] = jbtest(mors_vikt);
+[h4, p4] = jbtest(mors_langd);
+% Skriver ut resultatet för de fyra variablerna och kontrollera om de är normalfördelade vid signifikansnivån 5%.
+% Barnets vikt
+fprintf('Barnets födelsevikt: ');
+if p1 < alpha
+    fprintf('Inte normalfördelad (p-värde: %f)\n', p1);
+else
+    fprintf('Normalfördelad (p-värde: %f)\n', p1);
+end
+% Moderns ålder
+fprintf('Moderns ålder: ');
+if p2 < alpha
+    fprintf('Inte normalfördelad (p-värde: %f)\n', p2);
+else
+    fprintf('Normalfördelad (p-värde: %f)\n', p2);
+end
+% Moderns vikt
+fprintf('Moderns vikt: ');
+if p3 < alpha
+    fprintf('Inte normalfördelad (p-värde: %f)\n', p3);
+else
+    fprintf('Normalfördelad (p-värde: %f)\n', p3);
+end
+% Moderns längd
+fprintf('Moderns längd: ');
+if p4 < alpha
+    fprintf('Inte normalfördelad (p-värde: %f)\n', p4);
+else
+    fprintf('Normalfördelad (p-värde: %f)\n', p4);
+end
 
 %% Problem 6: Regression
 % res = w-X*beta_hat;
