d9ec6351a2cf832768d82de15a13572a558f25f5
Sara Rydell <sryde@kth.se>
2023-05-11 16:20:40 +0200

    Review of solutions

--- Lab2/Problem1.m
+++ Lab2/Problem1.m
@@ -1,34 +1,34 @@
 %% Problem 1: Simulering av konfidensintervall
-% Parametrar:
-n = 25; %Antal matningar
-mu = 2; %Vantevardet
-sigma = 1; %Standardavvikelsen
-alpha = 0.05;
-%Simulerar n observationer for varje intervall
-x = normrnd(mu, sigma,n,100); %n x 100 matris med varden
-%Skattar mu med medelvardet
-xbar = mean(x); %vektor med 100 medelvarden.
-%Beraknar de undre och ovre granserna
-undre = xbar - norminv(1-alpha/2)*sigma/sqrt(n);
-ovre = xbar + norminv(1-alpha/2)*sigma/sqrt(n);
+    % Parametrar:
+    n = 25; %Antal matningar
+    mu = 2; %Vantevardet
+    sigma = 1; %Standardavvikelsen
+    alpha = 0.05;
+    %Simulerar n observationer for varje intervall
+    x = normrnd(mu, sigma,n,100); %n x 100 matris med varden
+    %Skattar mu med medelvardet
+    xbar = mean(x); %vektor med 100 medelvarden.
+    %Beraknar de undre och ovre granserna
+    undre = xbar - norminv(1-alpha/2)*sigma/sqrt(n);
+    ovre = xbar + norminv(1-alpha/2)*sigma/sqrt(n);
 
 %% Problem 1: Simulering av konfidensintervall (forts.)
-%Ritar upp alla intervall
-figure(1)
-hold on
-for k=1:100
-if ovre(k) < mu % Rodmarkerar intervall som missar mu
-plot([undre(k) ovre(k)],[k k],'r')
-elseif undre(k) > mu
-plot([undre(k) ovre(k)],[k k],'r')
-else
-plot([undre(k) ovre(k)],[k k],'b')
-end
-end
-%b1 och b2 ar bara till for att figuren ska se snygg ut.
-b1 = min(xbar - norminv(1 - alpha/2)*sigma/sqrt(n));
-b2 = max(xbar + norminv(1 - alpha/2)*sigma/sqrt(n));
-axis([b1 b2 0 101]) %Tar bort outnyttjat utrymme i figuren
-%Ritar ut det sanna vardet
-plot([mu mu],[0 101],'g')
-hold off
+    %Ritar upp alla intervall
+    figure(1)
+    hold on
+    for k=1:100
+        if ovre(k) < mu % Rodmarkerar intervall som missar mu
+            plot([undre(k) ovre(k)],[k k],'r')
+            elseif undre(k) > mu
+                plot([undre(k) ovre(k)],[k k],'r')
+        else
+            plot([undre(k) ovre(k)],[k k],'b')
+        end
+    end
+    %b1 och b2 ar bara till for att figuren ska se snygg ut.
+    b1 = min(xbar - norminv(1 - alpha/2)*sigma/sqrt(n));
+    b2 = max(xbar + norminv(1 - alpha/2)*sigma/sqrt(n));
+    axis([b1 b2 0 101]) %Tar bort outnyttjat utrymme i figuren
+    %Ritar ut det sanna vardet
+    plot([mu mu],[0 101],'g')
+    hold off
--- Lab2/Problem2.m
+++ Lab2/Problem2.m
@@ -1,17 +1,17 @@
 %% Problem 2: Maximum likelihood/Minsta kvadrat
-M = 1e4;
-b = 4;
-x = raylrnd(b, M, 1);
-hist_density(x, 40)
-hold on
-my_est_ml = sqrt(sum(x.^2)/(2*M)); % Skriv in din ML-skattning har
-my_est_mk = sqrt(2/pi)*mean(x); % Skriv in din MK-skattning har
-% plot(my_est_ml, 0, 'r*');
-% plot(my_est_mk, 0, 'g*');
-% plot(b, 0, 'ro');
-% hold off
+    M = 1e4;
+    b = 4;
+    x = raylrnd(b, M, 1);
+    hist_density(x, 40)
+    hold on
+    my_est_ml = sqrt(sum(x.^2)/(2*M)); % Skriv in din ML-skattning har
+    my_est_mk = sqrt(2/pi)*mean(x); % Skriv in din MK-skattning har
+    plot(my_est_ml, 0, 'r*');
+    plot(my_est_mk, 0, 'g*');
+    plot(b, 0, 'ro');
+    % hold off
 
 %% Problem 2: Maximum likelihood/Minsta kvadrat (forts.)
-x_axis = 0:0.1:17; % tillagd för att kunna ändra intervallen
-plot(x_axis, raylpdf(x_axis, my_est_ml), 'r');
-hold off
+    x_axis = 0:0.1:17; % tillagd för att kunna ändra intervallen
+    plot(x_axis, raylpdf(x_axis, my_est_ml), 'r');
+    hold off
